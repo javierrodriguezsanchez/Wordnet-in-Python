0d3a64e7602a1aca4138cda1166d2276145b8576
javierrodriguezsanchez <105332166+javierrodriguezsanchez@users.noreply.github.com>
2023-11-10 01:34:57 -0500

    comentando el codigo

--- dic/views.py
+++ dic/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django import forms
-from nltk.corpus import wordnet
 from dic.getdata import getdata
 # Create your views here.
 
-class Entry(forms.Form):
+class Entry(forms.Form):#clase para permitir que el usuario entre la palabra
     entrada=forms.CharField(max_length=100, required=False, label='')
-    def __str__(self):
-        return '{}'.format(self.entrada)
 
 def hello(request):
+    
     word=Entry(request.POST)    
     contexto={'entrada':'','form':word}
-    if(request.method=="POST"):
+
+    if(request.method=="POST"):#mostrar resultados
         result=getdata(request.POST['entrada'])
         contexto={'entrada':result[0],'datos':result[1],'form':word}
         return render(request,'extends.html',contexto)
+    #mostrar la pantalla inicial
     return render(request,'index.html',contexto)
     
