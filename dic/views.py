0fcbe318d8f0143f9c9bc783f30c2a14f0a4276e
javierrodriguezsanchez <105332166+javierrodriguezsanchez@users.noreply.github.com>
2023-11-09 22:19:56 -0500

    Last details

--- dic/views.py
+++ dic/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django import forms
 from nltk.corpus import wordnet
+from dic.getdata import getdata
 # Create your views here.
 
 class Entry(forms.Form):
@@ -13,49 +14,8 @@ def hello(request):
     word=Entry(request.POST)    
     contexto={'entrada':'','form':word}
     if(request.method=="POST"):
-        result=datas(request.POST['entrada'])
+        result=getdata(request.POST['entrada'])
         contexto={'entrada':result[0],'datos':result[1],'form':word}
         return render(request,'extends.html',contexto)
     return render(request,'index.html',contexto)
     
-def gettype(a):
-    if a == 'n': return 'noun'
-    if a == 'v': return 'verb'
-    if a == 'r': return 'adverb'
-    return 'adjetive'
-
-def datas(input):
-    aux=input.replace(' ','_')
-    #nltk.download('wordnet')
-    info=[
-        {
-            'name':a.name(),
-            'type':gettype(a.pos()),
-            'meaning':a.definition(),
-            'Syn':list(filter(lambda x:x!=input, map(lambda x:x.name(),a.lemmas()))),
-            'AnySyn':any(filter(lambda x:x.name()!=input,a.lemmas())),
-            'Ant':list(map(lambda x:x.name(),a.lemmas()[0].antonyms())),
-            'AnyAnt':any(a.lemmas()[0].antonyms()),
-            'Hyper':list(map(lambda x:x.lemmas()[0].name(),a.hypernyms())),
-            'AnyHyper':any(a.hypernyms()),
-            'Hypo':list(map(lambda x:x.lemmas()[0].name(),a.hyponyms())),
-            'AnyHypo':any(a.hyponyms()),
-            'PHol':list(map(lambda x:x.lemmas()[0].name(),a.part_holonyms())),
-            'AnyPHol':any(a.part_holonyms()),
-            'PMer':list(map(lambda x:x.lemmas()[0].name(),a.part_meronyms())),
-            'AnyPMer':any(a.part_meronyms()),
-            'MHol':list(map(lambda x:x.lemmas()[0].name(),a.member_holonyms())),
-            'AnyMHol':any(a.member_holonyms()),
-            'MMer':list(map(lambda x:x.lemmas()[0].name(),a.member_meronyms())),
-            'AnyMMer':any(a.member_meronyms()),
-            'SMer':list(map(lambda x:x.lemmas()[0].name(),a.substance_meronyms())),
-            'AnySMer':any(a.substance_meronyms()),
-            'Ent':list(map(lambda x:x.lemmas()[0].name(),a.entailments())),
-            'AnyEnt':any(a.entailments()),
-            'Causes':list(map(lambda x:x.lemmas()[0].name(),a.causes())),
-            'AnyCauses':any(a.causes()),
-        }
-        for a in wordnet.synsets(aux)
-    ]
-
-    return [input,info]
